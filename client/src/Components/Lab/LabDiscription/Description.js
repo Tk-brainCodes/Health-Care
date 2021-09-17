1db5485b694a6087cdb88abfbf774f48de5af826
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-09-17 15:08:05 +0100

    added details

--- client/src/Components/Lab/LabDiscription/Description.js
+++ client/src/Components/Lab/LabDiscription/Description.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import './index.css';
 
 const Description = ({ lab }) => {
   return (
@@ -6,12 +7,26 @@ const Description = ({ lab }) => {
       <div className="lab__discription">
         <div className="lab__details">
           {lab ? (
-            <div>
-              Name: {lab.name} <br />
-              Rating: {lab.rating} <br />
-              Email: {lab.email}
-              <br />
-              Address: {lab.address.street_line_one}
+            <div className="book__test">
+              <h1 id="large__heading">{lab.name}</h1>
+              <div className="buttons__">
+                <div className="website">website</div>
+                <div className="call">call</div>
+                <div className="appointment">book appointment</div>
+              </div>
+              <p id="details"> <b>Address:</b> {lab.address.street_line_one}</p>
+              <p id="details"><b>Hours:</b>  open - Closes 8PM</p>
+              <p id="details"><b>Phone:</b> {lab.phone} </p>
+              <p id="details"><b>Email:</b> {lab.email}</p>
+              <div className="test__available">
+                <p id="test__heading">Available Tests</p>
+                <div className="buttons__">
+                  <div  className="test__tags">Blood Test</div>
+                  <div className="test__tags">HIV Test</div>
+                  <div className="test__tags">Sugar Test</div>
+                  <div className="test__tags">Heart Test</div>
+                </div>
+              </div>
             </div>
           ) : (
             <div>not available</div>
