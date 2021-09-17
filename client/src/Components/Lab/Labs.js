9da86f76e0ffd0293b51b1d3df4940663dd2a618
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-09-17 03:22:25 +0100

    added search and display results

--- client/src/Components/Lab/Labs.js
+++ client/src/Components/Lab/Labs.js
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import './Labs.css'
+import axios from 'axios'
 
 const Labs = () => {
+  const [labs, setLabs] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const apiURI = 'https://demo-api.pneumahealth.co/labs';
+  const fetchLabs = async () => {
+    const response = await axios.get(apiURI, { headers: { Accept: "application/json" } });
+    let userArray = [];
+    userArray.push(response.data.data);
+    console.log(response.data.data)
+    setLabs(userArray[0].toString());
+  }
+  useEffect(() => {
+    fetchLabs();
+  }, [apiURI])
+
+  const filterLabs = labs.filter((data) => {
+    let labName = data.name.toLowercase().includes(search.toLowercase());
+    let labLocation = data.country.toLowercase().includes(search.toLowercase());
+    return (
+      labName + labLocation
+    )
+  });
+
   return (
-    <div className='Labs__'>
-      <div className='find__labs'>
-      <div className="component1">
-      {/*Search bar*/}
+    <Fragment>
+      <div className="find__labs">
+        <div className="labs___section1">
+          <input
+            type="text"
+            placeholder="Search for lab or location"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div className="search__result">
+            {filterLabs.map((seen, i) => (
+              <div key={i}>
+                <h2>{seen.name}</h2>
+                <h3>{seen.city}, {seen.state}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="labs__section2">
+          {labs.map((lab, i) => (
+            <div className="lab__details" key={i}>
+              {lab.name}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="component2"></div>
-      </div>
-    </div>
+    </Fragment>
   )
 }
 export default Labs
